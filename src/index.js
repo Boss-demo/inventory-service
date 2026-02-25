e853c0acc27a03668041c1252926832e22bf247e
Lokesh Kumar <lokesh22ari@gmail.com>
2026-02-26 02:51:49 +0530

    feat: checking the updated boss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 console.log("updated the inventory..");
 console.log("Checking once for boss tt the inventory..");
+console.log("updated the inventory..2XX");
+console.log("Checking once for boss tt the inventory..");
